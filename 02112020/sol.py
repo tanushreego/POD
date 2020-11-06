4ee2f949b15cba1fdfc8ca9830ed5cc91d908de3
TANUSHREE GORAI <tanushreegorai@TANUSHREEs-MacBook-Pro.local>
2020-11-06 20:29:55 +0530

    inital commit

--- 02112020/sol.py
+++ 02112020/sol.py
@@ -3,8 +3,14 @@ n=int(input())
 m=int(input())
 a=m+2
 b=n-(m+a)
-
-while (b>a and m+a+b==n):
-    print(m,a,b)
-    a=a+2
-    b=n-m+a
+while(b>a and m+a+b==n):
+    m1=m
+    a1=a
+    b1=b
+    while ( b1 > a1 and m1 + a1 + b1 == n ) :
+        print (m1 , a1 , b1 )
+        a1 = a1 + 2
+        b1=n-(m1+a1)
+m+=2
+a=m+2
+b=n-(m+a)
